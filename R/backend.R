a657b97470025b9bba160713da50f2bd34f2b82a
Rolf Simoes <rolfsimoes@gmail.com>
2026-06-21 23:31:16 -0300

    Add generic backend system and job utilities

---
+++ R/backend.R
@@ -0,0 +1,34 @@
+.pump_resolve_backend <- function(backend) {
+    if (!is.character(backend)) {
+        return(backend)
+    }
+    switch(backend,
+        future = future_backend(),
+        mirai = mirai_backend(),
+        main = main_backend(),
+        default = main_backend(),
+        stop("invalid backend")
+    )
+}
+
+.pump_executor_count <- function(backend) {
+    UseMethod(".pump_executor_count")
+}
+.pump_executor_new_job <- function(backend, func, args) {
+    UseMethod(".pump_executor_new_job")
+}
+.pump_job_is_ready <- function(job) {
+    UseMethod(".pump_job_is_ready")
+}
+.pump_job_data <- function(job) {
+    UseMethod(".pump_job_data")
+}
+
+.make_job <- function(expr) {
+    start <- Sys.time()
+    val <- tryCatch(expr, error = function(e) {
+        class(e) <- c("pump_error", class(e))
+        e
+    })
+    list(value = val, fn_time = as.numeric(difftime(Sys.time(), start, units = "secs")) * 1000)
+}
